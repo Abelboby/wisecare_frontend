5f11368d472a2aed72dee4f721bdf2ad3659a433
abelboby <abeldavisboby@gmail.com>
2026-03-01 12:33:26 +0530

    Add extension for HomeScreenState with placeholder function for tab navigation.

---
+++ lib/ui/home/home_functions.dart
@@ -0,0 +1,7 @@
+part of 'home_screen.dart';
+
+extension _HomeScreenFunctions on _HomeScreenState {
+  // void onTabTapped(AppTab tab) {
+  //   context.read<HomeProvider>().switchTab(tab);
+  // }
+}
