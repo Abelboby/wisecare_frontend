fff3d52e43b20b42273a52e546f2d30401553a56
abelboby <abeldavisboby@gmail.com>
2026-03-01 12:33:34 +0530

    Add HomeScreen widget with tab navigation and content for Home and Settings tabs.

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../provider/home_provider.dart';
+import '../../enums/app_enums.dart';
+
+part 'home_functions.dart';
+part 'widgets/home_bottom_nav.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // void _refresh() => mounted ? setState(() {}) : null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Consumer<HomeProvider>(
+        builder: (context, provider, _) {
+          switch (provider.currentTab) {
+            case AppTab.home:
+              return const _HomeTabContent();
+            case AppTab.settings:
+              return const _SettingsTabContent();
+          }
+        },
+      ),
+      bottomNavigationBar: const _HomeBottomNav(),
+    );
+  }
+}
+
+class _HomeTabContent extends StatelessWidget {
+  const _HomeTabContent();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Home',
+        style: Theme.of(context).textTheme.headlineMedium,
+      ),
+    );
+  }
+}
+
+class _SettingsTabContent extends StatelessWidget {
+  const _SettingsTabContent();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Settings',
+        style: Theme.of(context).textTheme.headlineMedium,
+      ),
+    );
+  }
+}
