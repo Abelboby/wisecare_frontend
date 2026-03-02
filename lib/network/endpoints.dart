72dfb68aec24954d72c46896101f76381c18a4bb
abelboby <abeldavisboby@gmail.com>
2026-03-02 23:59:17 +0530

    Refactor: Rename authentication endpoint constants for clarity and consistency

--- lib/network/endpoints.dart
+++ lib/network/endpoints.dart
@@ -4,9 +4,9 @@ class Endpoints {
 
   static const String base = '';
   static const String auth = '/auth';
-  static const String authLogin = '/auth/login';
+  static const String authSignin = '/auth/signin';
   static const String authRefresh = '/auth/refresh';
-  static const String authLogout = '/auth/logout';
+  static const String authSignout = '/auth/signout';
   static const String user = '/user';
   static const String userProfile = '/user/profile';
 }
