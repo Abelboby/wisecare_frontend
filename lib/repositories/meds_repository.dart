bc44d40f3807d7d3b0135d721a6a7a54e4ff6f1e
abelboby <abeldavisboby@gmail.com>
2026-03-06 00:41:36 +0530

    Add: MedsRepository class for orchestrating medication data interactions with MedsService

---
+++ lib/repositories/meds_repository.dart
@@ -0,0 +1,22 @@
+import 'package:wisecare_frontend/models/meds/meds_schedule_model.dart';
+import 'package:wisecare_frontend/services/meds_service.dart';
+
+/// Meds data orchestration. Only this layer talks to MedsService.
+class MedsRepository {
+  MedsRepository({MedsService? medsService})
+      : _medsService = medsService ?? MedsService();
+
+  final MedsService _medsService;
+
+  Future<MedsScheduleModel> getSchedule({String? date}) async {
+    return _medsService.getSchedule(date: date);
+  }
+
+  Future<void> markAsTaken(String medicationId) async {
+    await _medsService.markAsTaken(medicationId);
+  }
+
+  Future<void> requestRefill(String medicationId) async {
+    await _medsService.requestRefill(medicationId);
+  }
+}
